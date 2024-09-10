bd6e5f202934367b278c690b419379652fb0b456
JOAO PEDRO ALMEIDA MENINO <00001132521804sp@aluno.educacao.sp.gov.br>
2024-09-10 17:38:47 -0300

    kwh e tempo

--- desconto.py
+++ desconto.py
@@ -1,9 +1,44 @@
-var = float(input("digite o preço: "))
-desconto = int(input("digite o numero do desconto SO O NUMERO SEM %: "))
+def pagar():
+    var = float(input("digite o preço: "))
+    desconto = int(input("digite o numero do desconto SO O NUMERO SEM %: "))
 
-var_desconto =  (var*desconto)/100
+    var_desconto =  (var*desconto)/100
 
-pagar = var-var_desconto
+    pagar = var-var_desconto
 
-print(f"vc tem um desconto de R$:{var_desconto}")
-print(f"o valor a pagar é {pagar}")
+    print(f"vc tem um desconto de R$:{var_desconto}")
+    print(f"o valor a pagar é {pagar}")
+
+def tempo():
+    distancia = int(input("digite a distancia: "))
+    velocidade = int(input("digite a velocidade media da rota: "))
+
+    tempo = distancia/velocidade
+
+    print(f" o tempo extimado é de: {tempo}")
+
+def kw():
+    kwh = int(input("digite os kwh: "))
+
+    tipo = input("digite o tipo C, R ou I: ").lower()
+
+    if tipo == "r":
+        if kwh > 500: 
+            print(kwh*0,65)
+        else:
+            print(kwh*0,40)
+    elif tipo == "c":
+        if kwh > 1000: 
+            print(kwh*0,60)
+        else:
+            print(kwh*0,55)
+    elif tipo == "i":
+        if kwh > 5000: 
+            print(kwh*0,60)
+        else:
+            print(kwh*0,55)
+    else:
+        print("ERRO")
+        
+        
+    
